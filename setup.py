ad1779291bbfa64e51754587c688c1fc9fd2e4d4
John Zhou <john.ly.zhou@gmail.com>
2021-12-29 14:45:50 -0800

    create proper directory structure on setup

--- setup.py
+++ setup.py
@@ -1,6 +1,24 @@
+import os
+from pathlib import Path
 from setuptools import find_packages, setup
+
 
 setup(
     name='src',
     packages=find_packages(),
 )
+
+# Set up directories
+repo_dir = os.path.dirname(os.path.realpath(__file__))
+
+data_dir = Path(f"{repo_dir}/data")
+raw_data_dir = Path(f"{data_dir}/raw")
+process_data_dir = Path(f"{data_dir}/processed")
+figures_dir = Path(f"{repo_dir}/figures")
+experiments_dir = Path(f"{repo_dir}/experiments")
+
+data_dir.mkdir(parents=True, exist_ok=True)
+raw_data_dir.mkdir(parents=True, exist_ok=True)
+process_data_dir.mkdir(parents=True, exist_ok=True)
+figures_dir.mkdir(parents=True, exist_ok=True)
+experiments_dir.mkdir(parents=True, exist_ok=True)
