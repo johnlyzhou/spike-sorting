d0071edd22e61556df40f10b61766d913fcbe1b9
John Zhou <john.ly.zhou@gmail.com>
2021-12-29 15:31:01 -0800

    include probe geometry file for example

--- setup.py
+++ setup.py
@@ -11,9 +11,8 @@ setup(
 # Set up directories
 repo_dir = os.path.dirname(os.path.realpath(__file__))
 
-dir_names = ["data", "data/raw", "data/processed", "figures", "experiments"]
+dir_names = ["data/processed", "figures", "experiments"]
 
 for dir_name in dir_names:
     dir_path = Path(f"{repo_dir}/{dir_name}")
     dir_path.mkdir(parents=True, exist_ok=True)
-
