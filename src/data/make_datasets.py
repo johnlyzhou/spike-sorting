bba79f742e4d1d639b93be07e8a9fe8387edeb32
John Zhou <john.ly.zhou@gmail.com>
2022-01-18 17:12:25 -0500

    add noise option for clustering and featurization datasets, remove training data normalization

--- src/data/make_datasets.py
+++ src/data/make_datasets.py
@@ -9,30 +9,46 @@ def dataset_saver(func):
         templates, predicted_ptps, positions, idx_units = func(*args, **kwargs)
 
         templates = time_center_templates(templates)
-        templates = normalize_inputs(templates)
+        # Want to keep information about PTP amplitude
+        # templates = normalize_inputs(templates)
 
         templates = templates.swapaxes(1, 2)
         positions = np.transpose(positions)
         experiment_dir = Path(f"{experiment_data_dir}/{experiment_name}")
         experiment_dir.mkdir(parents=True, exist_ok=True)
-        print("Saving templates to folder {}, array of size: {}".format(experiment_dir, templates.shape))
+        print("Saving templates to folder {}, array of shape: {}".format(experiment_dir, templates.shape))
         np.save("{}/templates.npy".format(experiment_dir), templates)
-        print("Saving predicted PTPs to folder {}, array of size: {}".format(experiment_dir, predicted_ptps.shape))
+        print("Saving predicted PTPs to folder {}, array of shape: {}".format(experiment_dir, predicted_ptps.shape))
         np.save("{}/predicted_ptps.npy".format(experiment_dir), predicted_ptps)
-        print("Saving positions to folder {}, array of size: {}".format(experiment_dir, positions.shape))
+        print("Saving positions to folder {}, array of shape: {}".format(experiment_dir, positions.shape))
         np.save("{}/positions.npy".format(experiment_dir), positions)
-        print("Saving unit indices to folder {}, array of size: {}".format(experiment_dir, idx_units.shape))
+        print("Saving unit indices to folder {}, array of shape: {}".format(experiment_dir, idx_units.shape))
         np.save("{}/unit_idxs.npy".format(experiment_dir), idx_units)
+
     return wrapper_dataset_saver
 
 
 @dataset_saver
-def featurization_dataset(templates, positions_templates, channels_pos, a, loc, scale, n_samples=10000):
+def featurization_dataset(templates, positions_templates, channels_pos, a, loc, scale, n_samples=10000,
+                          raw_rec_path=None):
     """
     Produces a dataset for feature learning. For each sample, randomly sample a template and position parameters
     x, y, z, and alpha, then use the point cloud model from Boussard et al. 2021 to project that template to a new
     location and estimate its new PTP amplitude on each recording channel.
     """
+    num_templates, num_timesteps, num_channels = templates.shape
+
+    if raw_rec_path:
+        print("Adding noise to dataset")
+        noise = np.load(raw_rec_path)
+        num_timesteps_noise, num_channels_noise = noise.shape
+        if num_timesteps_noise < num_timesteps:
+            raise ValueError(f"Recording needs to be at least {num_timesteps} timesteps for these templates.")
+        if num_channels_noise < num_channels:
+            raise ValueError(f"Recordings needs at least {num_channels} channels for these templates.")
+    else:
+        print("No noise to add to dataset")
+
     gamma = stats.gamma
     alpha = gamma.rvs(a, loc, scale, size=n_samples)
     y = np.random.uniform(0, 150, n_samples)
@@ -59,6 +75,14 @@ def featurization_dataset(templates, positions_templates, channels_pos, a, loc, 
     for i in tqdm(range(n_samples)):
         idx_temp = np.random.choice(np.arange(positions_templates.shape[0]))
         idx_units[i] = idx_temp
+
+        if raw_rec_path:
+            # Sample a bit of noise
+            noise_timesteps = np.random.randint(num_timesteps_noise - num_timesteps)
+            noise_chans = np.random.randint(num_channels_noise - num_channels)
+            sample_noise = noise[noise_timesteps:noise_timesteps + num_timesteps,
+                                 noise_chans:noise_chans + num_channels]
+
         for j in range(channels_pos.shape[0]):
             predicted_ptp = positions_templates[idx_temp, 3] / ((([positions_templates[idx_temp, 0],
                                                                    positions_templates[idx_temp, 1]] -
@@ -66,6 +90,9 @@ def featurization_dataset(templates, positions_templates, channels_pos, a, loc, 
                                                                 positions_templates[idx_temp, 2] ** 2) ** 0.5
             new_predicted_ptp = alpha[i] / (((x_z[:, i] - channels_pos[j]) ** 2).sum() + y[i] ** 2) ** 0.5
             new_templates[i, :, j] = templates[idx_temp, :, j] * new_predicted_ptp / predicted_ptp
+            if raw_rec_path:
+                new_templates[i, :, j] += sample_noise[:, j]
+
     return new_templates, new_templates.ptp(1), relocated_positions, idx_units
 
 
@@ -142,7 +169,7 @@ def positional_invariance_dataset(templates, positions_templates, channels_pos, 
 
 @dataset_saver
 def clustering_dataset(templates, positions_templates, channels_pos, a, loc, scale, n_clusters=20,
-                       num_samples_per_cluster=100):
+                       num_samples_per_cluster=100, raw_rec_path=None):
     """
     Produces a dataset for feature evaluation on evaluation performance. For each cluster, randomly sample a
     template and set of mean position parameters x, y, z, and alpha. Then add some Gaussian noise to each positional
@@ -152,9 +179,21 @@ def clustering_dataset(templates, positions_templates, channels_pos, a, loc, sca
     Note: unlike the featurization and positional invariance datasets, we do not (yet) conduct an "outside"
     position check after adding drift.
     """
-    if templates.shape[0] < n_clusters:
+    num_templates, num_timesteps, num_channels = templates.shape
+
+    if num_templates < n_clusters:
         raise ValueError("Too few templates available to create desired number of clusters.")
 
+    if raw_rec_path:
+        print("Adding noise to dataset")
+        noise = np.load(raw_rec_path)
+        num_timesteps_noise, num_channels_noise = noise.shape
+        if num_timesteps_noise < num_timesteps:
+            raise ValueError(f"Recording needs to be at least {num_timesteps} timesteps for these templates.")
+        if num_channels_noise < num_channels:
+            raise ValueError(f"Recordings needs at least {num_channels} channels for these templates.")
+    else:
+        print("No noise to add to dataset")
     gamma = stats.gamma
 
     # For each cluster, randomly select a mean position
@@ -183,29 +222,37 @@ def clustering_dataset(templates, positions_templates, channels_pos, a, loc, sca
     x_z = np.random.normal(mean_x_z, 20)
     y = np.random.normal(mean_y, 20)
 
-    new_templates = np.zeros((n_clusters * num_samples_per_cluster, templates.shape[1],
-                              templates.shape[2]))
+    new_templates = np.zeros((n_clusters * num_samples_per_cluster, num_timesteps,
+                              num_channels))
     relocated_positions = np.zeros((4, n_clusters * num_samples_per_cluster))
     relocated_positions[:2] = x_z
     relocated_positions[2] = y
     relocated_positions[3] = alpha
     idx_units = np.zeros(num_samples_per_cluster * n_clusters)
-    idx_temps = np.random.choice(np.arange(positions_templates.shape[0]), size=n_clusters, replace=False)
+    # Randomly sample templates
+    idx_temps = np.random.choice(np.arange(num_templates), size=n_clusters, replace=False)
 
     i = 0
     for k in tqdm(range(n_clusters)):
         idx_temp = idx_temps[k]
-        for j in range(num_samples_per_cluster):
+        for _ in range(num_samples_per_cluster):
             idx_units[i] = idx_temp
-            # scaling_factor = np.random.uniform(0.8, 1.5)
-            for j in range(channels_pos.shape[0]):
-                predicted_ptp = positions_templates[idx_temp, 3] / ((([positions_templates[idx_temp, 0],
-                                                                       positions_templates[idx_temp, 1]] -
+            # Randomly sample a bit of noise
+            noise_timesteps = np.random.randint(num_timesteps_noise - num_timesteps)
+            noise_chans = np.random.randint(num_channels_noise - num_channels)
+            sample_noise = noise[noise_timesteps:noise_timesteps + num_timesteps,
+                                 noise_chans:noise_chans + num_channels]
+            for j in range(num_channels):
+                predicted_ptp = positions_templates[idx_temp, 3] / (((positions_templates[idx_temp, :1] -
                                                                       channels_pos[j]) ** 2).sum() +
                                                                     positions_templates[idx_temp, 2] ** 2) ** 0.5
                 new_predicted_ptp = alpha[i] / (((x_z[:, i] - channels_pos[j]) ** 2).sum() + y[i] ** 2) ** 0.5
                 new_templates[i, :, j] = templates[idx_temp, :, j] * new_predicted_ptp / predicted_ptp
+
+                if raw_rec_path:
+                    new_templates[i, :, j] += sample_noise[:, j]
             i += 1
+
     return new_templates, new_templates.ptp(1), relocated_positions, idx_units
 
 
